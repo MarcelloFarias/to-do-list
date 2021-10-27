34064c0b4a22830dcbb8d24d2a8e09ee7e0565c5
Marcello <marcellorocha632@gmail.com>
2021-10-27 15:38:44 -0300

    Add the functionality to check a task

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ const btnAdd = document.querySelector('.btn-add');
 function createTask() {
     if (inputTask.value != ''){
         const task = document.createElement('li');
-        task.className = 'task';
+        task.classList.add('task');
         task.innerText = inputTask.value;
 
         const buttonsTask = document.createElement('div');
-        buttonsTask.className = 'buttons-task';
+        buttonsTask.classList.add('buttons-task');
 
         const btnDelete = document.createElement('button');
         btnDelete.innerText = 'Delete';
-        btnDelete.className = 'btn-delete';
+        btnDelete.classList.add('btn-delete');
 
         buttonsTask.appendChild(btnDelete);
 
@@ -25,6 +25,17 @@ function createTask() {
         btnDelete.addEventListener('click', function() {
             list.removeChild(task);
         });
+
+        task.addEventListener('click', function() {
+            if(task.className == 'task') {
+                task.classList.add('task-checked');
+                btnDelete.classList.add('btn-delete-checked');
+            }
+            else {
+                task.className = 'task';
+                btnDelete.className = 'btn-delete';
+            }
+        });    
     }
     else {
         alert("You didn't type any task");
